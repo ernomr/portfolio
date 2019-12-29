fbda64a3c4257513ff1d7055cf371ea3abe8e3b0
ErnoRatti <erno.matias@gmail.com>
2019-12-29 14:51:15 +0200

    Updates

---
+++ precache-manifest.a166609de91218cc5aa89fcfddca0f7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ee3c04c12e6a0319880ab810c54b1e",
+    "url": "/ernomr/portfolio.git/index.html"
+  },
+  {
+    "revision": "79f14eea7059838e4bf3",
+    "url": "/ernomr/portfolio.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ccc394772e74b6bca597",
+    "url": "/ernomr/portfolio.git/static/js/2.b5f86ef7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ernomr/portfolio.git/static/js/2.b5f86ef7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79f14eea7059838e4bf3",
+    "url": "/ernomr/portfolio.git/static/js/main.3cfc8515.chunk.js"
+  },
+  {
+    "revision": "f7f89db60e378d9d3c1c",
+    "url": "/ernomr/portfolio.git/static/js/runtime-main.c19621a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ernomr/portfolio.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
